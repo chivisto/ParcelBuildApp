b0641827cc043e8896097e637635cc124e65dd9c
Chivisto <isaaccovarr@gmail.com>
2022-03-06 19:08:01 -0700

    Using Parcel Now

---
+++ dist/index.579125c3.js
@@ -0,0 +1,77 @@
+const taskInput = document.getElementById("new-task");
+const addButton = document.getElementsByTagName("button")[0];
+const incompleteTasksHolder = document.getElementById("incomplete-tasks");
+const completedTasksHolder = document.getElementById("completed-tasks");
+//New Task List Item
+const createNewTaskElement = (taskString)=>{
+    let listItem = document.createElement("li");
+    let checkBox = document.createElement("input");
+    let label = document.createElement("label");
+    let editInput = document.createElement("input");
+    let editButton = document.createElement("button");
+    let deleteButton = document.createElement("button");
+    checkBox.type = "checkbox";
+    editInput.type = "text";
+    editButton.innerText = "Edit";
+    editButton.className = "edit";
+    deleteButton.innerText = "Delete";
+    deleteButton.className = "delete";
+    label.innerText = taskString;
+    listItem.appendChild(checkBox);
+    listItem.appendChild(label);
+    listItem.appendChild(editInput);
+    listItem.appendChild(editButton);
+    listItem.appendChild(deleteButton);
+    return listItem;
+};
+//Add a new task
+const addTask = ()=>{
+    let listItem = createNewTaskElement(taskInput.value);
+    incompleteTasksHolder.appendChild(listItem);
+    bindTaskEvents(listItem, taskCompleted);
+    taskInput.value = "";
+};
+//Edit an existing task
+const editTask = function() {
+    let listItem = this.parentNode;
+    let editInput = listItem.querySelector("input[type=text");
+    let label = listItem.querySelector("label");
+    let containsClass = listItem.classList.contains("editMode");
+    if (containsClass) label.innerText = editInput.value;
+    else editInput.value = label.innerText;
+    listItem.classList.toggle("editMode");
+};
+//Delete an existing task
+const deleteTask = function() {
+    let listItem = this.parentNode;
+    let ul = listItem.parentNode;
+    ul.removeChild(listItem);
+};
+//Mark a task as complete
+const taskCompleted = function() {
+    let listItem = this.parentNode;
+    completedTasksHolder.appendChild(listItem);
+    bindTaskEvents(listItem, taskIncomplete);
+};
+//Mark a task as incomplete
+const taskIncomplete = function() {
+    let listItem = this.parentNode;
+    incompleteTasksHolder.appendChild(listItem);
+    bindTaskEvents(listItem, taskCompleted);
+};
+const bindTaskEvents = (taskListItem, checkBoxEventHandler)=>{
+    let checkBox = taskListItem.querySelector("input[type=checkbox]");
+    let editButton = taskListItem.querySelector("button.edit");
+    let deleteButton = taskListItem.querySelector("button.delete");
+    editButton.onclick = editTask;
+    deleteButton.onclick = deleteTask;
+    checkBox.onchange = checkBoxEventHandler;
+};
+//Set the click handler to the addTask function
+addButton.addEventListener("click", addTask);
+//cycle over incompleteTasksHolder ul list items
+for(let i = 0; i < incompleteTasksHolder.children.length; i++)bindTaskEvents(incompleteTasksHolder.children[i], taskCompleted);
+//cycle over completedTasksHolder ul list items
+for(let i1 = 0; i1 < completedTasksHolder.children.length; i1++)bindTaskEvents(completedTasksHolder.children[i1], taskIncomplete);
+
+//# sourceMappingURL=index.579125c3.js.map
